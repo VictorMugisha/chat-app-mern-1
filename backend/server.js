d02a95497437658e198a3bfcd372eee9193df4f2
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-09 07:59:22 +0200

    Installed nodemon and created simple API endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { chats } = require("./data/data");
+const chats = require("./data/data.js");
 
 const app = express();
 
 app.get("/api/chat", (req, res) => {
-  res.send(chats);
+  res.status(200).json(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+    const id  = req.params.id;
+    const singleChat = chats.find(chat => chat._id === id);
+    res.send(singleChat)
 });
 
 app.listen(5000, () => {
