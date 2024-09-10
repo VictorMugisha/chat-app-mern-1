280c80789b87132a232fcca523ca60c3dd2a121b
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-10 10:22:32 +0200

    Generate token

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,4 +1,41 @@
+const expressAsyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const generateToken = require("../config/generateToken");
 
-async function registerUser() {
+const registerUser = expressAsyncHandler(async (req, res) => {
+  const { name, email, password, picture } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please Enter all the Feilds");
+  }
 
-}
+  // check if user already exists
+  const user = await User.findOne({ email });
+  if (user) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Create a new user
+  const newUser = await User.create({
+    name,
+    email,
+    password,
+    picture,
+  });
+
+  if (newUser) {
+    res.status(201).json({
+      _id: newUser._id,
+      name: newUser.name,
+      email: newUser.email,
+      picture: newUser.picture,
+      token: generateToken(newUser._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Failed to create user");
+  }
+});
+
+module.exports = { registerUser };
