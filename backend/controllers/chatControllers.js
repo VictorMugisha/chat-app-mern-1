a4ef2cb61b0343428a65abb96800fa4c6300100d
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-11 08:32:46 +0200

    Created handler for fetching chats that a logged in user belongs to

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -49,4 +49,24 @@ const accessChat = expressAsyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { accessChat };
+const fetchChats = expressAsyncHandler(async (req, res) => {
+    try {
+        Chat.find({users: {$elemMatch: {$eq: req.user._id}}})
+            .populate("users", "-password")
+            .populate("groupAdmin", "-password")
+            .populate("latestMessage")
+            .sort({updatedAt: -1})
+            .then(async (results) => {
+                results = await User.populate(results, {
+                    path: "latestMessage.sender",
+                    select: "name picture email",
+                });
+                res.status(200).send(results);
+            });
+    } catch (error) {
+        res.status(400);
+        throw new Error(error.message);
+    }
+})
+
+module.exports = { accessChat, fetchChats };
