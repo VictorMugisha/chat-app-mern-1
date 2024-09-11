690df5b50109a192d22288f628853fdccdd0e1ce
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-11 07:30:15 +0200

    Login form is also working

--- frontend/src/components/Authentication/Login.jsx
+++ frontend/src/components/Authentication/Login.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import {
   Button,
   FormControl,
@@ -6,26 +5,85 @@ import {
   Input,
   InputGroup,
   InputRightElement,
+  useToast,
   VStack,
 } from "@chakra-ui/react";
 import { useState } from "react";
+import axios from "axios";
+// import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const [show, setShow] = useState(false);
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const toast = useToast();
+
+  // const history = useHistory();
+  const navigate = useNavigate();
 
   function handleClick() {
     setShow(!show);
   }
-  async function submitHandler() {}
+  async function submitHandler() {
+    setLoading(true);
+    if (!email || !password) {
+      toast({
+        title: "Please Fill all the Feilds",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        "/api/user/login",
+        { email, password },
+        config
+      );
+      toast({
+        title: "Login Successful",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      // history.push("/chats");
+      navigate("/chats", { replace: true });
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description:
+          error.response?.data?.message ||
+          "Something went wrong. Please try again.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+  }
 
   return (
     <VStack spacing="10px">
       <FormControl id="email" isRequired>
         <FormLabel>Email Address</FormLabel>
         <Input
+          autoFocus
           value={email}
           type="email"
           placeholder="Enter Your Email Address"
