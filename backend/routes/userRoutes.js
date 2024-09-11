6e246c644792de99ee32cb616fcb69780c7304a9
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-11 07:55:34 +0200

    Created authorization middleware to allow access to logged in users only

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,8 +5,9 @@ const {
   authUser,
   allUsers,
 } = require("../controllers/userControllers");
+const { protect } = require("../middleware/authMiddleware");
 
-router.route("/").post(registerUser).get(allUsers);
+router.route("/").post(registerUser).get(protect, allUsers);
 router.post("/login", authUser);
 
 module.exports = router;
