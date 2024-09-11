ef6045126dc7a08ae80b061e400ddd7ceb7783b2
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-11 19:56:21 +0200

    Tried to implement SideDrawer component

--- frontend/src/components/miscellaneous/SideDrawer.jsx
+++ frontend/src/components/miscellaneous/SideDrawer.jsx
@@ -1,5 +1,14 @@
 /* eslint-disable no-unused-vars */
-import { Box, Button, Text, Tooltip } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  Text,
+  Tooltip,
+} from "@chakra-ui/react";
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import { useState } from "react";
 
 export default function SideDrawer() {
@@ -10,13 +19,46 @@ export default function SideDrawer() {
 
   return (
     <>
-      <Box>
+      <Box
+        d="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="white"
+        w="100%"
+        p="5px 10px 5px 10px"
+        borderWidth="5px"
+      >
         <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
-          <Button variant={"ghost"}>
-            <i className="fa fa-search" aria-hidden="true"></i>
-            <Text d={{ base: "none", md: 'flex' }} px={'4px'}>Search Users</Text>
+          <Button variant="ghost">
+            <i className="fas fa-search"></i>
+            <Text d={{ base: "none", md: "flex" }} px={4}>
+              Search User
+            </Text>
           </Button>
         </Tooltip>
+
+        <Text fontSize="2xl" fontFamily="Work sans">
+          Talk-A-Tive
+        </Text>
+
+        <div>
+          <Menu>
+            <MenuButton p={1} bg="white" as={Button}>
+              <BellIcon fontSize="2xl" m={1} />
+            </MenuButton>
+            <MenuList></MenuList>
+          </Menu>
+
+          <Menu>
+            <MenuButton
+              as={Button}
+              bg="white"
+              rightIcon={<ChevronDownIcon />}
+            >
+              
+            </MenuButton>
+          </Menu>
+        </div>
       </Box>
     </>
   );
