bd30d7e4645972200edd7ca190e5d689b1afb66f
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-11 08:23:35 +0200

    Created a route handler to create or access one on one chat

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db.js");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes.js");
+const chatRoutes = require("./routes/chatRoutes.js");
 
 const { notFound, errorHandler } = require("./middleware/errorMiddleware.js");
 
@@ -14,17 +15,8 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/api/chat", (req, res) => {
-  res.status(200).json(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  const id = req.params.id;
-  const singleChat = chats.find((chat) => chat._id === id);
-  res.send(singleChat);
-});
-
 app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
